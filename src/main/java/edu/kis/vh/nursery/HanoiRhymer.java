59197a0a7c4129e2982e94726c6de766a0ac2aa3
student <student@L404-2.carbon.cti.p.lodz.pl>
2022-04-11 10:54:08 +0200

    2. Poprawiono bledy z formatowaniem kodu

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,17 +1,22 @@
 package edu.kis.vh.nursery;
 
+// Złe były wiersze zawierające pole totalRejected oraz instrukcje warunkowe w metodzie countIn
+
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+    int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+    }
 }
+
+
+// Kombinacja ALT + strzałka zmienia widok na nastepny lub poprzedni plik
