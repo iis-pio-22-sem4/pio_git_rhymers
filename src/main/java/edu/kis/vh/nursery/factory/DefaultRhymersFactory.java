d7686c9a8307dc094ac789f403496861f6f18e3f
Mateusz Cybulski <235847@edu.p.lodz.pl>
2022-04-12 10:42:51 +0200

    2. Edycja gałęzi format

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -7,24 +7,27 @@ import edu.kis.vh.nursery.factory.Rhymersfactory;
 
 public class DefaultRhymersFactory implements Rhymersfactory {
 
-	@Override
-	public defaultCountingOutRhymer GetStandardRhymer() {
-		return new defaultCountingOutRhymer();
-	}
+    @Override
+    public defaultCountingOutRhymer GetStandardRhymer() {
+        return new defaultCountingOutRhymer();
+    }
 
-	@Override
-	public defaultCountingOutRhymer GetFalseRhymer() {
-		return new defaultCountingOutRhymer();
-	}
+    @Override
+    public defaultCountingOutRhymer GetFalseRhymer() {
+        return new defaultCountingOutRhymer();
+    }
 
-	@Override
-	public defaultCountingOutRhymer GetFIFORhymer() {
-		return new FIFORhymer();
-	}
+    @Override
+    public defaultCountingOutRhymer GetFIFORhymer() {
+        return new FIFORhymer();
+    }
 
-	@Override
-	public defaultCountingOutRhymer GetHanoiRhymer() {
-		return new HanoiRhymer();
-	}
+    @Override
+    public defaultCountingOutRhymer GetHanoiRhymer() {
+        return new HanoiRhymer();
+    }
 
 }
+
+//alt -> - przejście do najbliższego na pasku otwartych plików pliku po prawej stronie
+//alt <- - przejście do najbliższego na pasku otwartych plików pliku po lewej stronie
