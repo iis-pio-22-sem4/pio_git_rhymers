e657daa288685795b1362e0dec7a35a038c6ab3e
cpprian <cpprian456@gmail.com>
2022-04-13 13:02:14 +0200

    modify formatting code

--- src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
@@ -2,14 +2,19 @@ package edu.kis.vh.nursery.factory;
 
 import edu.kis.vh.nursery.defaultCountingOutRhymer;
 
-	public interface Rhymersfactory {
-	
-		public defaultCountingOutRhymer GetStandardRhymer();
-		
-		public defaultCountingOutRhymer GetFalseRhymer();
-		
-		public defaultCountingOutRhymer GetFIFORhymer();
-		
-		public defaultCountingOutRhymer GetHanoiRhymer();
-		
-	}
+public interface Rhymersfactory {
+
+	public defaultCountingOutRhymer GetStandardRhymer();
+
+	public defaultCountingOutRhymer GetFalseRhymer();
+
+	public defaultCountingOutRhymer GetFIFORhymer();
+
+	public defaultCountingOutRhymer GetHanoiRhymer();
+
+}
+
+// efekt działania kombinacji klawiszów alt + <-/->
+// powoduje zmianę otwartej na widoku strony
+// przechodzenie pomiędzy otwrtymi plikami do edycji w IDE
+// należy mieć otwarte przynajmniej dwa pliki do wizualnego efektu
