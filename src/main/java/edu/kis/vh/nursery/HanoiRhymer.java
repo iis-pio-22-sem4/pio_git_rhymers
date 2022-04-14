02dc1e82dfadf0bf7ac85d8b3995fa798f13f81f
235943 <235943@CARBON.CTI.P.LODZ.PL>
2022-04-14 10:53:40 +0200

    2- formatowanie kodu

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,20 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+    int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+    }
 }
+
+//5, 12-16
+// alt + <- oraz alt + -> powoduje zmiane obecnie otwartego pliku na inny który
+// tez jest otwarty w zakladce
