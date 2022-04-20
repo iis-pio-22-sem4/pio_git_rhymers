c472b1ddab2d67298284ece2879607b1ccff6cb6
235910 <235910@CARBON.CTI.P.LODZ.PL>
2022-04-20 13:28:48 +0200

    6. Ustawienie mdyfikatorow final

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -7,7 +7,7 @@ public class DefaultCountingOutRhymer {
     public static final int ERROR = -1;
     public static final int FULL = 11;
 
-    private int[] numbers = new int[SIZE];
+    private final int[] numbers = new int[SIZE];
 
     public int total = -1;
 
