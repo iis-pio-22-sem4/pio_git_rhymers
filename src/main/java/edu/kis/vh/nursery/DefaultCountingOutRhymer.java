acba0e734f4926c4fd22a883f16bcee4bcab99ec
Jakub Więcek <71319403+Kubator00@users.noreply.github.com>
2022-04-20 13:25:09 +0200

    6. ustawienie modyfikatora final

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -5,7 +5,7 @@ public class DefaultCountingOutRhymer {
 	public static final int ARRAY_CAPACITY = 12;
 	public static final int ARRAY_EMPTY = -1;
 	public static final int ARRAY_FULL = 11;
-	private int[] numbers = new int[ARRAY_CAPACITY];
+	private final int[] numbers = new int[ARRAY_CAPACITY];
 
 	
 	public int total = ARRAY_EMPTY;
