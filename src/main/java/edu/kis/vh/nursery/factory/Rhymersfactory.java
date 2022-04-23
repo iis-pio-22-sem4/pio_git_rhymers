feeacfd3f9dd85447c74c017a2c43c56a434f151
MateuszAdamski <235823@edu.p.lodz.pl>
2022-04-23 23:59:22 +0200

    2 utworzenie galezi format oraz poprawienie formatowania

--- src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
@@ -2,14 +2,15 @@ package edu.kis.vh.nursery.factory;
 
 import edu.kis.vh.nursery.defaultCountingOutRhymer;
 
-	public interface Rhymersfactory {
-	
-		public defaultCountingOutRhymer GetStandardRhymer();
-		
-		public defaultCountingOutRhymer GetFalseRhymer();
-		
-		public defaultCountingOutRhymer GetFIFORhymer();
-		
-		public defaultCountingOutRhymer GetHanoiRhymer();
-		
-	}
+public interface Rhymersfactory {
+
+	public defaultCountingOutRhymer GetStandardRhymer();
+
+	public defaultCountingOutRhymer GetFalseRhymer();
+
+	public defaultCountingOutRhymer GetFIFORhymer();
+
+	public defaultCountingOutRhymer GetHanoiRhymer();
+
+}
+//kombinacja klawiszy działa poprawnie - służy do przełączania się pomiędzy otwartymi oknami w środowisku programistycznym
