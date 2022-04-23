be8a0d09ecd619dcb629685497adf9eedc245003
Laura Szarzyńska <235987@edu.p.lodz.pl>
2022-04-23 14:41:11 +0200

    zad2 - branch format

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,20 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+	int totalRejected = 0;
 
 	public int reportRejected() {
 		return totalRejected;
 	}
 
 	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
+		if (!callCheck() && in > peekaboo())
 			totalRejected++;
-			else
-				super.countIn(in);
+		else
+			super.countIn(in);
 	}
 }
+//zle sformatowane to: wiersz 5, 12, 14, 15
+// teoretycznie moznaby powiedziec, ze wiersz 13 rowniez, ale poprawa 12 sprawia, ze 13 jest poprawny
+
+//alt+-> pozwala na łatwe przeskakiwanie pomiędzy otwartymi w IDE plikami
