57207093c7a45a5814ee9ef92abe218a8928d9dc
Małgorzata Sieńska <235965@edu.p.lodz.pl>
2022-04-25 10:59:24 +0200

    5. Wygenerowanie stalych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,11 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+	public static final int SIZE = 12;
+	public static final int TOTAL_START = -1;
+	private int[] numbers = new int[SIZE];
 
-	public int total = -1;
+	public int total = TOTAL_START;
 
 	public void countIn(int in) {
 		if (!isFull())
