29411692a3b3d2e902bd943e4ef602339b7da0b8
235887 <235887@CARBON.CTI.P.LODZ.PL>
2022-04-27 13:43:05 +0200

    12. Rozbicie metody main

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -12,28 +12,31 @@ class RhymersDemo {
 	public static void main(String[] args) {
 		Rhymersfactory factory = new DefaultRhymersFactory();
 
+		testRhymers(factory);
+
+	}
+
+	private static void testRhymers(Rhymersfactory factory) {
 		DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
 				factory.getFIFORhymer(), factory.getHanoiRhymer()};
-
 
 
 		for (int i = 1; i < MAX_I; i++)
 			for (int j = 0; j < MAX_J; j++)
 				rhymers[j].countIn(i);
-		
+
 		java.util.Random rn = new java.util.Random();
 		for (int i = 1; i < MAX_I; i++)
 			rhymers[MAX_J].countIn(rn.nextInt(BOUND));
-		
+
 		for (int i = 0; i < rhymers.length; i++) {
 			while (!rhymers[i].callCheck())
 				System.out.print(rhymers[i].countOut() + "  ");
 			System.out.println();
 		}
-		
+
 		System.out.println("total rejected is "
 				+ ((HanoiRhymer) rhymers[MAX_J]).reportRejected());
-		
 	}
-	
+
 }
