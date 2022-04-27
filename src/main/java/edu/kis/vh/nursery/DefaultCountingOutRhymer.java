93aabe4560a483401644fdf0f6c57986daee1f6a
daradramat <dadusiag@gmail.com>
2022-04-27 21:35:54 +0200

    8. Zmiana widoczności atrybutów klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -7,7 +7,7 @@ public class DefaultCountingOutRhymer {
     public static final int MAX_STACK_CAPACITY = 11;
     private final int[] numbers = new int[NUMBERS_ARR_INIT_SIZE];
 
-    public int total = ERR_CODE;
+    private int total = ERR_CODE;
 
     public void countIn(int in) {
         if (!isFull())
