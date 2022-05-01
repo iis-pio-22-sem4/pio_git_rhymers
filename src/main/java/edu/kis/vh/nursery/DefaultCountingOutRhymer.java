cee882370fc01d992f039d5b5a9eb9e308419837
TheWeebsSlayer <66646362+TheWeebsSlayer@users.noreply.github.com>
2022-05-01 22:51:19 +0200

    Zadanie 9 generowanie settera i gettera

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -13,12 +13,20 @@ public class DefaultCountingOutRhymer {
             numbers[++total] = in;
     }
 
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
     public boolean callCheck() {
         return total == INITIAL_COUNT;
     }
 
     public boolean isFull() {
         return total == SIZE - 1;
+    }
+
+    public int getTotal() {
+        return total;
     }
 
     protected int callPeekaboo() {
