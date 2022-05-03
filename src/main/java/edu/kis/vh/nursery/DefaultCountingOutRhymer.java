090121e05cbb348cdfc793c32542be827c03209a
Martyna <235887@edu.p.lodz.pl>
2022-05-03 22:16:47 +0200

    16. Klasyfikacja klasy DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,9 @@
 package edu.kis.vh.nursery;
 
+
+/**
+ * Class to count out numbers
+ */
 public class DefaultCountingOutRhymer {
 
 
@@ -10,26 +14,40 @@ public class DefaultCountingOutRhymer {
     private final int[] numbers = new int[SIZE];
 
     private int total = -1;
-    
+
+    /**
+     * @param in Function fills the numbers with the parameter in
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * @return Function check if the table is empty
+     */
     public boolean callCheck() {
         return total == ERROR;
     }
 
+    /**
+     * @return Function check if the table is full
+     */
     public boolean isFull() {
         return total == FULL;
     }
 
+    /**
+     * @return Returns the last element
+     */
     protected int peekaboo() {
         if (callCheck())
             return ERROR;
         return numbers[total];
     }
-
+    /**
+     * @return Return last element and decrease total number of elements
+     */
     public int countOut() {
         if (callCheck())
             return ERROR;
