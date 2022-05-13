7832d2b74c4fca72b737ea6df8407023fea350ab
Kasia Jankowska <katarzyna.jankowska000728@gmail.com>
2022-05-13 20:57:55 +0200

    13.2 Usuniecie pliku Node i przebudowanie klasy IntLinkedList tak by zawierała klasę Node

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -4,12 +4,12 @@ public class IntLinkedList {
 
 	private static class Node {
 
-		public int value;
-		public Node prev, next;
+		private final int value;
+		private Node prev;
+		private Node next;
 		public Node(int i) {
 			value = i;
 		}
-
 	}
 
 	Node last;
